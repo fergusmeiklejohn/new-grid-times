40cc793f59354b4108f025b22934fd7b19aea2f7
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2022-09-22 16:04:30 +0800

    footer complete

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import styled from "styled-components/macro";
+import { QUERIES } from "../../constants";
 
 const SectionTitle = ({ children, cornerLink }) => {
   return (
