386cf64e5b2af6c1fa4d717e518f74de0382c8c4
Fergus Meiklejohn <fergusmeiklejohn@gmail.com>
2022-09-22 16:06:40 +0800

    tweaks

--- src/components/SectionTitle/SectionTitle.js
+++ src/components/SectionTitle/SectionTitle.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 import styled from "styled-components/macro";
-import { QUERIES } from "../../constants";
 
 const SectionTitle = ({ children, cornerLink }) => {
   return (
